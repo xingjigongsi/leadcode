069b3303d6a359b8fce09b4df16609826cf14463
xingjigongsi <xingji123.ok@163.com>
2020-07-16 09:45:29 +0800

    fixed

---
+++ 动态规划/96. 不同的二叉搜索树.go
@@ -0,0 +1,16 @@
+package leadcode
+
+func numTrees(n int) int {
+	if n==0{
+		return 0
+	}
+	dp:= make([]int,n+1)
+	dp[0] = 1
+	dp[1] =1
+	for i:=2;i<=n;i++{
+		for j:=1;j<=i;j++{
+			dp[i]+= dp[j-1]*dp[i-j]
+		}
+	}
+	return dp[n+1]
+}
