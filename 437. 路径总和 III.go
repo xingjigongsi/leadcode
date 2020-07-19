7a8ea02ec7713accc2b43ae22c1902d6f79627b4
xingjigongsi <xingji123.ok@163.com>
2020-07-20 01:38:19 +0800

    fixed

--- 437. 路径总和 III.go
+++ 437. 路径总和 III.go
@@ -9,5 +9,24 @@ package leadcode
  * }
  */
 func pathSum(root *TreeNode, sum int) int {
+	if root == nil{
+		return 0
+	}
+	res:=pathHelper(root,sum)
+	res+=pathSum(root.Left,sum)
+	res+=pathSum(root.Right,sum)
+	return res
+}
 
+func pathHelper(root *TreeNode, sum int) int{
+	if root == nil{
+		return 0
+	}
+	res:= 0
+	if root.Val == sum{
+		res+=1
+	}
+	res+=pathHelper(root.Left,sum-root.Val)
+	res+=pathHelper(root.Right,sum-root.Val)
+	return res
 }
