0d3871b19f7c260debfe2e3fc26746f9cd0cf9f6
xingjigongsi <xingji123.ok@163.com>
2020-07-23 18:17:22 +0800

    fixed

--- 动态规划/55. 跳跃游戏.go
+++ 动态规划/55. 跳跃游戏.go
@@ -1,1 +1,16 @@
 package leadcode
+
+
+func canJump(nums []int) bool {
+	nums_len:= len(nums)
+	if nums_len==0{
+		return false
+	}
+	cat_index:=nums_len-1;
+	for i:= nums_len-1;i>=0;i--{
+		if nums[i]+i>=cat_index{
+			cat_index = i
+		}
+	}
+	return cat_index==0
+}
