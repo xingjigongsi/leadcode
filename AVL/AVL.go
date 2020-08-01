90920684374fc3d838ae2885a029316ad5ce52e1
xingjigongsi <xingji123.ok@163.com>
2020-08-01 12:23:57 +0800

    fixed

--- AVL/AVL.go
+++ AVL/AVL.go
@@ -1,8 +1,6 @@
 package AVL
 
-import (
-	"math"
-)
+import "math"
 
 type avl struct {
 	key int
@@ -20,6 +18,33 @@ func InitAvl(key int,value string) *avl{
 	}
 }
 
+/**
+* 右旋转
+*/
+func (this *avl) rightRoute(node *avl) *avl{
+	x:=node.left
+	t3:=x.right
+	x.right = node
+	node.left = t3
+	node.height = max(this.getNodeHeight(node.left),this.getNodeHeight(node.right))+1
+	x.height = max(this.getNodeHeight(x.left),this.getNodeHeight(x.right))+1
+	return  x
+}
+
+/**
+*
+ */
+ func (this *avl) leftRoute(node *avl) *avl{
+ 	x:=node.right
+ 	t3:=x.right
+ 	x.left  = node
+ 	node.right = t3
+ 	node.height = max(this.getNodeHeight(node.left),this.getNodeHeight(node.right))+1
+ 	x.height = max(this.getNodeHeight(x.left),this.getNodeHeight(x.right))+1
+	 return x
+ }
+
+ // 增加节点
 func (this *avl) Add(key int,value string) *avl{
 	if this==nil{
 		return &avl{
@@ -37,8 +62,11 @@ func (this *avl) Add(key int,value string) *avl{
 	}
 	//更新节点高度
 	this.height = max(this.getNodeHeight(this.left),this.getNodeHeight(this.right))+1
-	if this.getHeightDifference(this)>1{
-
+	if this.getHeightDifference(this)>1 && this.getHeightDifference(this.left)>=0{
+		return this.rightRoute(this)
+	}
+	if this.getHeightDifference(this)>1 && this.getHeightDifference(this.right)>=0{
+		return this.leftRoute(this)
 	}
 	return this
 }
@@ -85,6 +113,9 @@ func (this *avl) getNodeHeight(node *avl) int{
 
 //左右高度差
 func (this *avl) getHeightDifference(node *avl) int {
+	if node==nil{
+		return  0
+	}
 	return abs(this.getNodeHeight(node.left) - this.getNodeHeight(node.right))
 }
 
