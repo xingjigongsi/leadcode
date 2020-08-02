f43798431ed77bc16c5a688b6c10efae616fbfd7
xingjigongsi <xingji123.ok@163.com>
2020-08-02 18:45:08 +0800

    快慢指正

---
+++ link/面试题 02.08. 环路检测.go
@@ -0,0 +1,33 @@
+package link
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func detectCycle(head *ListNode) *ListNode {
+	if head ==nil || head.Next==nil{
+		return nil
+	}
+	slow:=head
+	fast:=head
+	for fast!=nil &&  fast.Next!=nil{
+		slow = slow.Next
+		fast = fast.Next.Next
+		if slow==fast{
+			break
+		}
+	}
+	if slow!=fast{
+		return nil
+	}
+	fast = head;
+	for slow!=fast{
+		slow = slow.Next
+		fast = fast.Next
+	}
+	return slow
+
+}
